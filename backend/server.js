4a5f2ad31125354429e369325aeee611238129a1
fedoraAnjara <hei.fedora.3@gmail.com>
2024-08-26 05:48:41 +0300

    fix: resolve bug with fetch data from server.js, change server port to 5000

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,44 @@
 // server.js
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// Example data
-const data = {
-    personne: {
-        nom: 'John Doe',
-    },
-    patrimoine: {
-        possessions: [
-            {
-                libelle: 'MacBook Pro',
-                valeur: 2500000,
-                dateDebut: '2023-01-01',
-                dateFin: null,
-                tauxAmortissement: 10,
-            },
-            {
-                libelle: 'Alternance',
-                valeurConstante: 500000,
-                dateDebut: '2022-01-01',
-                dateFin: null,
-                tauxAmortissement: 0,
-                jour: 1,
-            },
-            {
-                libelle: 'Survie',
-                valeurConstante: 300000,
-                dateDebut: '2022-05-01',
-                dateFin: null,
-                tauxAmortissement: 0,
-                jour: 2,
-            },
-        ],
-    },
-};
+const port = 5000;
+const cors = require('cors');
+app.use(cors());
 
 app.get('/api/data', (req, res) => {
+
+    const data = {
+        personne: {
+            nom: 'John Doe',
+        },
+        patrimoine: {
+            possessions: [
+                {
+                    libelle: 'MacBook Pro',
+                    valeur: 2500000,
+                    dateDebut: '2023-01-01',
+                    dateFin: null,
+                    tauxAmortissement: 5,
+                },
+                {
+                    libelle: 'Alternance',
+                    valeurConstante: 500000,
+                    dateDebut: '2022-01-01',
+                    dateFin: null,
+                    tauxAmortissement: 0,
+                    jour: 1,
+                },
+                {
+                    libelle: 'Survie',
+                    valeurConstante: 300000,
+                    dateDebut: '2022-05-01',
+                    dateFin: null,
+                    tauxAmortissement: 0,
+                    jour: 2,
+                },
+            ],
+        },
+    };
     res.json(data);
 });
 
