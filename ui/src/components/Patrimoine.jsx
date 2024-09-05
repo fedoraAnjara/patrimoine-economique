20170be6691e566b32e9edcf253064309e9b15a7
fedoraAnjara <hei.fedora.3@gmail.com>
2024-09-05 14:03:32 -0400

    fix: remplace chart Bar by chart line, add the result input

--- ui/src/components/Patrimoine.jsx
+++ ui/src/components/Patrimoine.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
@@ -17,7 +18,8 @@ import '../assets/Patrimoine.css';
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
@@ -33,12 +35,46 @@ const Patrimoine = () => {
       {
         label: "Valeur du Patrimoine",
         data: [],
-        backgroundColor: "#a07e0d",
-        borderColor: "rgb(75, 192, 192)",
-        borderWidth: 1,
+        backgroundColor: "rgba(160, 126, 13, 0.2)",
+        borderColor: "#a07e0d",
+        borderWidth: 2,
+        tension: 0.4,  // Pour lisser la ligne
+        fill: true,    // Remplir la zone sous la courbe
+        pointRadius: 0, // Supprimer les points
+        pointHoverRadius: 0, // Supprimer les points au survol
       },
     ],
   });
+  
+  const [resultat, setResultat] = useState('');
+
+  const handleCalculate = async () => {
+    if (dateFin) {
+      const date = new Date(dateFin);
+      if (isNaN(date.getTime())) {
+        setResultat('Format de Date non valide!');
+        return;
+      }
+      
+      try {
+        const response = await fetch(`http://localhost:3000/patrimoine/${date.toISOString().split('T')[0]}`);
+        const result = await response.json();
+        
+        if (result.valeurPatrimoine !== undefined) {
+          // Arrondir à deux chiffres après la virgule
+          const valeurArrondie = parseFloat(result.valeurPatrimoine).toFixed(2);
+          setResultat(`La valeur du patrimoine à la date sélectionnée est de ${valeurArrondie} Ar`);
+        } else {
+          setResultat('Erreur dans le calcul du patrimoine.');
+        }
+      } catch (error) {
+        console.error('Erreur lors du calcul du patrimoine:', error);
+        setResultat('Erreur lors du calcul du patrimoine.');
+      }
+    } else {
+      setResultat('Date de fin non définie');
+    }
+  };
 
   const handleGetValeur = async () => {
     try {
@@ -61,9 +97,13 @@ const Patrimoine = () => {
           {
             label: "Valeur du Patrimoine",
             data: values,
-            backgroundColor: "#a07e0d",
-            borderColor: "rgb(75, 192, 192)",
-            borderWidth: 1,
+            backgroundColor: "rgba(160, 126, 13, 0.2)",
+            borderColor: "#a07e0d",
+            borderWidth: 2,
+            tension: 0.4, // Pour lisser la ligne
+            fill: true,
+            pointRadius: 0,  // Supprimer les points
+            pointHoverRadius: 0,  // Supprimer les points au survol
           },
         ],
       };
@@ -100,11 +140,25 @@ const Patrimoine = () => {
           ))}
         </select>
       </div>
+      {/* Button to calculate patrimoine */}
+      <button className="btn-submit" onClick={handleCalculate}>
+        Calculer le patrimoine
+      </button>
+
+      {/* Display patrimoine result in input with placeholder */}
+      <input
+        type="text"
+        value={resultat}
+        readOnly
+        placeholder="Valeur du patrimoine"
+        className="resultat-input"
+      />
+
       <button className="btn-submit" onClick={handleGetValeur}>
         Valider
       </button>
       <div className="chart-container">
-        <Bar
+        <Line
           data={data}
           options={{
             responsive: true,
@@ -122,10 +176,10 @@ const Patrimoine = () => {
             },
             scales: {
               x: {
-                stacked: true,
+                stacked: false,
               },
               y: {
-                stacked: true,
+                stacked: false,
               },
             },
           }}
